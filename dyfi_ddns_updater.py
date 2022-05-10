e3d6b75126ed762a5e0fe132057fc85a43c67f0a
Johan Penttinen <johanlepenttinen@gmail.com>
2022-05-10 17:03:37 +0300

    Fix 3->6 day update interval

--- dyfi_ddns_updater.py
+++ dyfi_ddns_updater.py
@@ -36,10 +36,11 @@ args = parser.parse_args()
 username = args.username
 password = args.password
 hostname = args.hostname
-UPDATE_INTERVAL = 4380
+# Dy.fi documentation deletes the IP if not updated in 6-7 days.
+# 8640 = 6 days
+UPDATE_INTERVAL = 8640
 CHECKIP_URL = "https://icanhazip.com"
 dyndns_url = f"{args.ddns}{hostname}"
-# Dy.fi documentation says it deletes the IP if not updated in 6-7 days.
 
 
 def get_ip():
@@ -48,12 +49,15 @@ def get_ip():
 
 
 def dns_update():
+    # GET request to the ddns by running curl in a shell.
     run(f"curl -D - --user '{username}:{password}' '{dyndns_url}'", shell=True)
+    # Logs each IP update to a file
     run("date >> /var/log/dyfi.log", shell=True)
     # run(f"echo '{username}:{password}' '{dyndns_url}'", shell=True)
 
 
 def main():
+    # +- 60 minutes
     randomized_interval = UPDATE_INTERVAL - random.randint(-60, 60)
     count = 1
     stored_ip = ""  # get_ip()
@@ -64,7 +68,7 @@ def main():
             dns_update()
             randomized_interval = UPDATE_INTERVAL - random.randint(-60, 60)
             count = 0
-
+        # Wait 2 minutes until next check
         time.sleep(120)
         count += 1
 
